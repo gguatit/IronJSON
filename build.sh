b2e941d54d43794df7e690ccd5dab61a1f190a99
gguatit <kako34133413@gmail.com>
2026-04-17 10:44:50 +0900

    feat: 빌드 스크립트 추가 및 메인 파일 경로 수정

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! command -v cargo &>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable --profile minimal
+    source "$HOME/.cargo/env"
+fi
+
+rustup target add wasm32-unknown-unknown
+
+cargo build --target wasm32-unknown-unknown --release
